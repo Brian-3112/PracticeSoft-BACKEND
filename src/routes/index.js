d49fae0bd6f3c204c9bbeabf5ea84a8836226058
Your Name <youexample.com>
2025-04-09 22:24:11 -0500

    comentar codgio 1

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,10 +4,12 @@ const routerUsuario  = require('./usuarioRouter');
 
 
 
-
+// configura las rutas principales de la API
 function routerApi(app){
     const router = express.Router();
+    // todas las rutas que se definan aquí empezarán con "/api"
     app.use("/api", router)
+    // hace que las rutas definidas en 'usuarioRouter' estén disponibles en "/api/usuarios"
     router.use('/usuarios', routerUsuario);
     
 }
