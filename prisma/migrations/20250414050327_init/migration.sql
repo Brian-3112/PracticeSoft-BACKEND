52dc54c3db552e28b0d9968f199469ebb84fdf5c
Your Name <youexample.com>
2025-04-14 00:03:59 -0500

    creacion tabla vehiculos

---
+++ prisma/migrations/20250414050327_init/migration.sql
@@ -0,0 +1,19 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `email` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `User_email_key`(`email`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Vehiculo` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nombreVehiculo` VARCHAR(191) NOT NULL,
+    `placa` VARCHAR(191) NOT NULL,
+    `transito` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
