bebc991e2622f1c59deabaf91121a696a93c9635
Brian Pareja <129825533+Brian-3112@users.noreply.github.com>
2026-05-05 00:08:57 -0500

    Remove password reset backend logic and related routes

--- src/routes/usuarioRouter.js
+++ src/routes/usuarioRouter.js
@@ -1,16 +1,11 @@
 const express = require('express');
-const { loginUser, registerUser, consulta, forgotPassword, resetPassword } = require('../controllers/usuarioController');
+const { loginUser, registerUser, consulta } = require('../controllers/usuarioController');
 
 const router = express.Router();
 
 
 //iniciar sesion, autenticar user
 router.post("/login", loginUser);
-router.post("/forgot-password", forgotPassword);
-router.post("/olvide-password", forgotPassword);
-router.post("/reset-password", resetPassword);
-router.post("/reset-password/:token", resetPassword);
-router.post("/nuevo-password/:token", resetPassword);
 //get
 router.get("/", consulta);
 //inser one
