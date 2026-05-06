b9e36076163f31e1b1e038cab6e407f82014e9fd
Your Name <youexample.com>
2026-05-06 02:59:16 -0500

    melo

--- app.js
+++ app.js
@@ -11,10 +11,7 @@ app.use(express.json());
 const port = process.env.PORT || 4000;
 
 
-app.get("/debug-users", async (req, res) => {
-    const users = await prisma.user.findMany();
-    res.json(users);
-});
+
 ///Middlewares
 app.use(cors());
 
