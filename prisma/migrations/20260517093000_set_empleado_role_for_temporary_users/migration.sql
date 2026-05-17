73a70370e2d2f3da7c59b98c4dca7c2307ae032a
Your Name <youexample.com>
2026-05-17 02:30:04 -0500

    modificaciones

--- prisma/migrations/20260517093000_set_empleado_role_for_temporary_users/migration.sql
+++
@@ -1,5 +0,0 @@
--- Normalize temporary-user role naming to 'empleado'
-UPDATE `User`
-SET `role` = 'empleado'
-WHERE `isTemporary` = true
-  AND (`role` IS NULL OR `role` = '' OR LOWER(`role`) = 'temporal');
